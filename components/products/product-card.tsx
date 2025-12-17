5628565814fdb5bb9f759ea9ea94d30876e4b202
StenixDev <stenix.dev@gmail.com>
2025-12-17 15:43:30 +0800

    feat: product card add more design

--- components/products/product-card.tsx
+++ components/products/product-card.tsx
@@ -7,6 +7,9 @@ import {
   CardTitle,
 } from '../ui/card';
 import { Badge } from '../ui/badge';
+import { ChevronDownIcon, ChevronUpIcon, StarIcon } from 'lucide-react';
+import { Button } from '../ui/button';
+import { cn } from '@/lib/utils';
 
 interface Product {
   id: number;
@@ -18,15 +21,58 @@ interface Product {
 }
 
 function ProductCard({ product }: { product: Product }) {
+  const hasVoted = false;
+
   return (
     <Link href={`/products/${product.id}`}>
-      <Card>
-        <CardHeader>
-          <CardTitle>{product.name}</CardTitle>
+      <Card className='group card-hover hover:bg-primary-foreground/10 border-solid border-gray-400 min-h-[180px]'>
+        <CardHeader className='flex-1'>
+          <div className='flex items-start gap-4'>
+            <div className='flex-1 min-w-0'>
+              <div className='flex items0center gap-2'>
+                <CardTitle className='text-lg group-hover:text-primary'>
+                  {product.name}
+                </CardTitle>
 
-          {product.isFeatured && <Badge>Featured</Badge>}
+                {product.isFeatured && (
+                  <Badge className='gap-1 bg-primary text-primary-foreground'>
+                    <StarIcon className='size-3 fill-current' />
+                    Featured
+                  </Badge>
+                )}
+              </div>
 
-          <CardDescription>{product.description}</CardDescription>
+              <CardDescription>{product.description}</CardDescription>
+            </div>
+            {/* {Voting buttons} */}
+
+            <div className='flex flex-col items-center gap-1 shrink-0'>
+              <Button
+                variant={'ghost'}
+                size='icon-sm'
+                className='h-8 w-8 text-primary hover:bg-primary/20'
+              >
+                <ChevronUpIcon className='size-5' />
+              </Button>
+              <span className='text-sm font-semibold transition-colors text-foreground'>
+                10
+              </span>
+
+              <Button
+                variant={'ghost'}
+                size='icon-sm'
+                // className='h-8 w-8 text-primary hover:text-destructive'
+                className={cn(
+                  'h-8 w-8 text-primary',
+                  hasVoted
+                    ? 'hover:text-destructive'
+                    : 'opacity-50 cursor-not-allowed'
+                )}
+              >
+                <ChevronDownIcon className='size-5' />
+              </Button>
+            </div>
+          </div>
         </CardHeader>
 
         <CardFooter>
