ee755dd3567f813d71c130ebe4cd88485ff0f053
StenixDev <stenix.dev@gmail.com>
2026-01-07 16:55:02 +0800

    completed [simple-contact-form]

--- simple-contact-form/components/contact-form.tsx
+++ simple-contact-form/components/contact-form.tsx
@@ -28,6 +28,7 @@ import {
   InputGroupText,
   InputGroupTextarea,
 } from '@/components/ui/input-group';
+import { createContact } from '@/server/contact';
 
 const formSchema = z.object({
   email: z.email(),
@@ -46,23 +47,15 @@ export function ContactForm() {
     },
   });
 
-  function onSubmit(data: z.infer<typeof formSchema>) {
-    toast('You submitted the following values:', {
-      description: (
-        <pre className='bg-code text-code-foreground mt-2 w-[320px] overflow-x-auto rounded-md p-4'>
-          <code>{JSON.stringify(data, null, 2)}</code>
-        </pre>
-      ),
-      position: 'top-center',
-      classNames: {
-        content: 'flex flex-col gap-2',
-      },
-      style: {
-        '--border-radius': 'calc(var(--radius)  + 4px)',
-      } as React.CSSProperties,
-    });
+  async function onSubmit(data: z.infer<typeof formSchema>) {
+    try {
+      await createContact(data.email, data.message);
+      toast.success('Contact created successfully');
+      form.reset();
+    } catch {
+      toast.error('Failed to create contact');
+    }
   }
-
   return (
     <Card className='w-full sm:max-w-md'>
       <CardHeader>
